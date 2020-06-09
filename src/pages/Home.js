0d7bccef6e9c1ac8996744c7af5ac22698ae3e83
SeanHeidrick <seanheidrick1@gmail.com>
2020-06-09 16:46:37 -0500

    Getting setup for React Router

---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+export const Home = () => {
+  let styles = {
+    color: "white",
+  };
+  return <div style={styles}>Home</div>;
+};
---
+++ src/pages/MyResume.js
@@ -0,0 +1,7 @@
+import React from "react";
+export const MyResume = () => {
+  let styles = {
+    color: "white",
+  };
+  return <div style={styles}>MyResume</div>;
+};
