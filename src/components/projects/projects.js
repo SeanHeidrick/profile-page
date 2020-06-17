1c6ecc8d0e83d0fded8eae32f3ff7d8ddfb560bd
SeanHeidrick <seanheidrick1@gmail.com>
2020-06-17 18:02:31 -0500

    feat: added todo list to projects page

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,4 +1,15 @@
 import React from "react";
+import todoList from "./img/todoList.jpg";
+import { TodoApp } from "./todoList/TodoApp";
+import "./projects.scss";
 export const Projects = () => {
-  return <div className="page-info">Projects</div>;
+  return (
+    <>
+      <h3 className="project-title">Projects</h3>
+      <div className="projects">
+        <TodoApp />
+      </div>
+      ;
+    </>
+  );
 };
