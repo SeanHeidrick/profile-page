56781b12b3bdb54c6477a087e43fdd4023877a4a
SeanHeidrick <seanheidrick1@gmail.com>
2020-06-18 16:38:56 -0500

    feat: added tip calculator to projects page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.scss";
 import { AboutMe } from "./components/aboutMe/aboutMe";
@@ -26,4 +27,5 @@ function App() {
     </>
   );
 }
+
 export default App;
